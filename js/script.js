709aae79f469be145e473f3df91e831d527e0d3c
rezvee99 <mdrezvee@gmail.com>
2020-06-15 15:15:25 +0600

    First Commit

---
+++ js/script.js
@@ -0,0 +1,54 @@
+/* ============================================
+|   |   |   |   Preloader  
+=============================================== */
+$(window).on('load', function () { //make sure that whole site is loaded
+    $('#status').fadeOut();
+    $('#preloader').delay(350).fadeOut('slow');
+});
+
+/* ============================================
+|   |   |   |   Team 
+=============================================== */
+
+$(function () {
+    $("#team-members").owlCarousel({
+        items: 2,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fa fa-angle-left"></i>',
+            '<i class="fa fa-angle-right"></i>'
+        ]
+    });
+
+});
+
+/* ============================================
+|   |   |   |   Progress Bars
+=============================================== */
+
+$(function () {
+
+    $("#progress-elements").waypoint(function () {
+
+        $(".progress-bar").each(function () {
+
+            $(this).animate({
+                width: $(this).attr("aria-valuenow") + "%"
+
+            }, 1000);
+
+        });
+
+        this.destroy();
+
+    }, {
+        offset: 'bottom-in-view'
+    });
+
+    /* */
+
+});
