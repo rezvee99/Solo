25e0371acfae61da3806b5bca69d0ebc96111485
rezvee99 <mdrezvee@gmail.com>
2020-07-27 08:41:13 +0600

    improved some layout for extra small devices

--- js/script.js
+++ js/script.js
@@ -162,7 +162,22 @@ $(function () {
         autoplayHoverPause: true,
         nav: true,
         dots: false,
-        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        responsive: {
+            // breakpoint from 0 up
+            0: {
+                items: 2
+            },
+            // breakpoint from 480 up
+            480: {
+                items: 3
+            },
+            // breakpoint from 768 up
+            768: {
+                items: 6
+            }
+
+        }
     });
 
 });
